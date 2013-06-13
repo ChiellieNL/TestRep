c4b9aa28c924fffaf27af8408088c5deec4581bd
Michiel Horvers <git@mhcp-software.nl>
2013-06-14 00:10:38 +0200

    Added Test Unit

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTest.h
+//  GuildBrowser
+//
+//  Created by Michiel Horvers on 14-06-13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
