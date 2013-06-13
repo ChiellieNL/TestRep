c054c9b3900880d614db1d8149a04318ffcaaa3b
Michiel Horvers <git@mhcp-software.nl>
2013-06-14 00:53:39 +0200

    Updated Test Units

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Michiel Horvers on 14-06-13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
